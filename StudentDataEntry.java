623070f2d1da10f25d717cc5172546b34bd5668e
Hadeseek <135782679+Hadeseek@users.noreply.github.com>
2025-04-18 23:49:51 +0530

    Create StudentDataEntry.java

---
+++ StudentDataEntry.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+
+public class StudentDataEntry {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Example of different functionalities
+        // Call functions to add students, display them, etc.
+    }
+    
+    // Function 1: Add student data
+    public static void addStudent() {
+        // logic for adding student data
+    }
+
+    // Function 2: Display student data
+    public static void displayStudent() {
+        // logic for displaying student data
+    }
+
+    // Function 3: Delete student data
+    public static void deleteStudent() {
+        // logic for deleting student data
+    }
+
+    // Other functions as required...
+}
